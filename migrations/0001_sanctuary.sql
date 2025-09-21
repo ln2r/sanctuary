fc2b336d6f85df441a64fa99a21d915c69045ddb
ln2r <scorch_62@live.com>
2025-09-21 21:55:14 +0700

    chore - update field sorting

--- migrations/0001_sanctuary.sql
+++ migrations/0001_sanctuary.sql
@@ -1,12 +1,12 @@
 -- Migration number: 0001 	 2025-08-17T05:39:59.584Z
 CREATE TABLE images  (
       id TEXT PRIMARY KEY NOT NULL,
-      created TEXT NOT NULL,
-      updated TEXT NOT NULL,
-      deleted TEXT,
       captured TEXT NOT NULL,
       published TEXT,
       path TEXT NOT NULL,
       caption TEXT,
-      views INTEGER DEFAULT 0
+      views INTEGER DEFAULT 0,
+      created TEXT NOT NULL,
+      updated TEXT NOT NULL,
+      deleted TEXT
 );
